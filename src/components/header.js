8416ca800856394ba5e5b22c9a059c5b0fb7594e
MacBook <macbookpro@Macbook-Pro.local>
2020-11-19 11:41:56 -0500

    Primitive Video Library - added shop button

--- src/components/header.js
+++ src/components/header.js
@@ -6,12 +6,14 @@ import Logo from '../assets/primitive_video.svg';
 const Header = () =>{
     return (
         <>
-            <Flex h='125px' fontFamily='Oswald' borderBottom='1px solid #999'>
-                <Flex bg='rgba(255,255,255,0.7)' alignItems='center' justifyContent='center' flexDir='column' w="100%" >
-                    <Link href='/' title="Home">
+            <Flex  fontFamily='Oswald' borderBottom='1px solid #999'>
+                <Flex bg='rgba(255,255,255,0.7)' alignItems='center' justifyContent='center' flexDir='column' w="100%" paddingBottom={['25px','25px', '0', '0']} >
+                    <Link href='/' title="Home" marginBottom={['0','0','15px','15px']}>
                         <Image src={Logo} w='200px' alignItems='center' />
                     </Link>
-                    
+                    <Button pos={['relative','relative','absolute','absolute']} target='_blank' colorScheme='yellow' top={['10px','10px','auto','auto']} right={['0','0','20px','20px']} fontSize='14px' textTransform='uppercase' fontWeight='normal'  as={GatsbyLink} to='https://primitiveskate.com'>
+                        Shop Primitiveskate.com
+                    </Button>
                 </Flex>
             </Flex>
         </>
