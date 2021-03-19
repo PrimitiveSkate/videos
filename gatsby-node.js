7469f231855b3f781789f61d433500740d300a0d
primitivearto <arthur.sousa@primitiveskate.com>
2021-03-19 14:53:35 -0400

    added shopify store fton api to allow users to see products related to videos by tags

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,34 +1,24 @@
-const Promise = require('bluebird');
-const path = require('path');
-
 exports.createPages = async ({graphql, actions}) => {
-    const result = await graphql(
-        `
-            {
-                allContentfulVideo {
-                    nodes {
-                        slugs
-                        id
-                    }
-                }
-            }
-        `
-    );
+  const result = await graphql(
+    `
+      {
+        allContentfulVideo {
+          nodes {
+            slugs
+            id
+          }
+        }
+      }
+    `
+  );
 
-    result.data.allContentfulVideo.nodes.forEach((post, index, posts) => {
-        actions.createPage({
-            path: `/video/${post.slugs}/`,
-            component: require.resolve('./src/templates/video-page'),
-            context: {
-                id: post.id,
-            }
-        });
-        // actions.createPage({
-        //     path: `/video/${post.slugs}/player`,
-        //     component: require.resolve('./src/templates/video-player'),
-        //     context: {
-        //         id: post.id,
-        //     }
-        // });
+  result.data.allContentfulVideo.nodes.forEach(async post => {
+    actions.createPage({
+      path: `/video/${post.slugs}/`,
+      component: require.resolve('./src/templates/video-page'),
+      context: {
+        id: post.id
+      }
     });
+  });
 };
